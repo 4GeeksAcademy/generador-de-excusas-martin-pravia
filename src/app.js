8830b68cd1f2b5ac871fc08aaee76bfa61462f8a
martopravia <marto.pravia@gmail.com>
2024-10-31 13:47:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let one = Math.floor(Math.random() * who.length);
+  let two = Math.floor(Math.random() * action.length);
+  let three = Math.floor(Math.random() * what.length);
+  let four = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[one] + action[two] + what[three] + when[four];
+
   console.log("Hello Rigo from the console!");
 };
